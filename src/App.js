21d6d4840916d71b75c613db4f6668d13b4beca6
devkdh91 <devkdh91@gmail.com>
2020-06-22 21:17:56 +0900

    할일 추가 기능까지 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
+const OuterWrap = styled.div`
+  width:100%;
+  height:100%;
+  background-color: rgba(0,128,0, 0.2);
+`
+const InnerWrap = styled.div`
+  width:60%;
+  padding:2rem;
+  margin:0 auto;
+`
+const DoDoTitle = styled.div`
+  font-size:4rem;
+  font-weight:bold;
+  text-align:center;
+`
+const TodayDate = styled.div`
+
+  font-size:2rem;
+  text-align:center;
+ `
 function App() {
+
+  const [dodolist, setDodolist] = useState([])
+  const [inputs, setInputs] = useState('')
+  const handleInput = (e) => {
+    setInputs(e.target.value)
+  }
+  const AddDodolist = () => {
+    setDodolist([...dodolist, inputs])
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <OuterWrap>
+      <InnerWrap>
+        <DoDoTitle>DO DO LIST</DoDoTitle>
+        <TodayDate>{new Date().toLocaleDateString()}</TodayDate>
+        <input placeholder="할 일 목록" onBlur={handleInput} />
+        <button onClick={AddDodolist}>추가</button>
+        <div>할일 목록
+          <ul>
+            {dodolist.length !== 0 && dodolist.map((f, idx) => <li key={idx}>{f}</li>)}
+          </ul>
+        </div>
+      </InnerWrap>
+    </OuterWrap>
   );
 }
 
